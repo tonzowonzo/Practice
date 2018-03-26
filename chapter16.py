3e32da8e2e464aab7731caabbd92705f53ee1805
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-03-26 20:19:58 +0200

    policy gradients.

--- chapter16.py
+++ chapter16.py
@@ -17,7 +17,9 @@ def plot_animation(frames, repeat=False, interval=40):
     fig = plt.figure()
     patch = plt.imshow(frames[0])
     plt.axis('off')
-    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
+    ani = animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
+    return ani
+
 # To plot pretty figures and animations
 import matplotlib
 import matplotlib.animation as animation
@@ -275,3 +277,139 @@ frames = render_policy_net('./my_policy_net_basic.ckpt', action, X)
 video = plot_animation(frames)
 plt.show()
 
+# Policy gradients.
+'''
+1 - Let the NN play the game several times and at each step
+compute the gradients that would make the chosen action even
+more likely, but don't apply these gradients yet.
+
+2 - Once you have run several episodes, compute each actions
+score using discounted rewards.
+
+3 - If an actions score is positive it means that the action was
+good and you want to apply the gradients computed earlier to make
+the action even more likely to be chosen in the future. However,
+if the score is negative, it means the action was bad and you want
+to apply the opposite gradients to make this action slightly less
+likely in the future. The solution is simply multiply each gradient 
+vector by the corresponding action score.
+
+4 - Finally, compute the mean of all the resulting gradient vectors,
+and use it to perform a gradient descent step.
+'''
+
+import tensorflow as tf
+
+reset_graph()
+
+n_inputs = 4
+n_hidden = 4
+n_outputs = 1
+
+learning_rate = 0.01
+
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+
+# Network creation.
+hidden = tf.layers.dense(X, n_hidden, activation=tf.nn.elu, kernel_initializer=initializer)
+logits = tf.layers.dense(hidden, n_outputs)
+outputs = tf.nn.sigmoid(logits)
+p_left_and_right = tf.concat(axis=1, values=[outputs, 1 - outputs])
+action = tf.multinomial(tf.log(p_left_and_right), num_samples=1)
+
+# Target function.
+y = 1. - tf.to_float(action) # Max probability of 1.
+
+# Cost function
+learning_rate = 0.01
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y,
+                                                        logits=logits)
+# Optimizer.
+optimizer = tf.train.AdamOptimizer(learning_rate)
+# Use compute_gradients because we want to tweak the gradients before we apply them.
+grads_and_vars = optimizer.compute_gradients(cross_entropy)
+
+# Gradients list.
+gradients = [grad for grad, variable in grads_and_vars]
+
+# Now we need to run the policy for a few episodes and then tweak the gradients.
+# To hold gradient vectors.
+gradient_placeholders = []
+grads_and_vars_feed = []
+for grad, variable in grads_and_vars:
+    gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
+    gradient_placeholders.append(gradient_placeholder)
+    grads_and_vars_feed.append((gradient_placeholder, variable))
+
+# Optimizer
+training_op = optimizer.apply_gradients(grads_and_vars_feed)
+
+# Initialise variables.
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+# Now we need functions to compute the total discounted rewards, given the raw rewards and normalise them.
+def discount_rewards(rewards, discount_rate):
+    discounted_rewards = np.empty(len(rewards))
+    cumulative_rewards = 0
+    for step in reversed(range(len(rewards))):
+        cumulative_rewards = rewards[step] + cumulative_rewards * discount_rate
+        discounted_rewards[step] = cumulative_rewards
+    return discounted_rewards
+
+# Function for normalisation of rewards.
+def discount_and_normalize_rewards(all_rewards, discount_rate):
+    all_discounted_rewards = [discount_rewards(rewards, discount_rate) for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - reward_mean)/reward_std for discounted_rewards in all_discounted_rewards]
+
+# Variables.
+n_iterations = 250
+n_max_steps = 1000
+n_games_per_update = 10 # Train the policy every 10 episodes.
+save_iterations = 10 # Save the model every 10 iterations.
+discount_rate = 0.95
+
+# Run the session.
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_iterations):
+        print("\rIteration: {}".format(iteration), end="")
+        all_rewards = []
+        all_gradients = []
+        for game in range(n_games_per_update):
+            current_rewards = []
+            current_gradients = []
+            obs = env.reset()
+            for step in range(n_max_steps):
+                action_val, gradients_val = sess.run([action, gradients], feed_dict={X: obs.reshape(1, n_inputs)})
+                obs, reward, done, info = env.step(action_val[0][0])
+                current_rewards.append(reward)
+                current_gradients.append(gradients_val)
+                if done:
+                    break
+            all_rewards.append(current_rewards)
+            all_gradients.append(current_gradients)
+
+        all_rewards = discount_and_normalize_rewards(all_rewards, discount_rate=discount_rate)
+        feed_dict = {}
+        for var_index, gradient_placeholder in enumerate(gradient_placeholders):
+            mean_gradients = np.mean([reward * all_gradients[game_index][step][var_index]
+                                      for game_index, rewards in enumerate(all_rewards)
+                                          for step, reward in enumerate(rewards)], axis=0)
+            feed_dict[gradient_placeholder] = mean_gradients
+        sess.run(training_op, feed_dict=feed_dict)
+        if iteration % save_iterations == 0:
+            saver.save(sess, "./my_policy_net_pg.ckpt")
+env.close()
+
+# Render policy net.
+frames = render_policy_net("./my_policy_net_pg.ckpt", action, X, n_max_steps=1000)
+video = plot_animation(frames)
+plt.show()
+
+# Markov chains.
